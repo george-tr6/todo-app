1bf6c1a5ab6aceebce8e0a0ed6b6c8cd50f8b2b0
george-tr6 <george.tr6@gmail.com>
2018-09-19 16:32:00 -0700

    Completed add functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,44 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
+import React, {Component} from 'react';
 import List from './list';
+import AddItem from './add_item'
+import dummyListData from '../dummy_data/list_data'
 
-const App = () => (
+class App extends Component{
+    state = {
+        list: []
+    }
+
+    componentDidMount(){
+        this.getListData();
+    }
+
+    getListData(){
+        //Call server to get data
+
+        this.setState({
+            list:dummyListData
+        });
+    }
+
+    addItem = (item)=>{
+        item._id = new Date().getTime();
+
+        this.setState({
+            list: [item, ...this.state.list]
+        });
+
+    }
+    render(){
+        const {list} = this.state;
+        return(
     <div className="container">
     <h1 className="center">To Do App</h1>
-            <List/>
+            <AddItem add={this.addItem}/>
+            <List data={list}/>
     </div>
-);
+        );
+    }
+}
 
 export default App;
